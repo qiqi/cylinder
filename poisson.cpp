fbc7db90abf677698994dbd72f09a896e11c6f62
Qiqi <qiqi@mit.edu>
2019-04-13 22:18:17 -0700

    Conversion to c++, builds

--- poisson.cpp
+++ poisson.cpp
@@ -21,7 +21,7 @@ float laplace_point(float * p, float * c) {
 }
 
 __device__
-float laplace_point_iter(float * p, float * c, float * bxArea,
+float laplace_point_iter(float * p, float * c, float bxArea,
                          float relaxation) {
     float res = laplace_point(p, c) - bxArea;
     return p[0] + relaxation * res / (c[0] + c[1] + c[2] + c[3]);
@@ -41,9 +41,34 @@ void laplace_iter_i(float * pNext, float * p, float * b,
     float pStencil[5] = {p0,
           ir < nr - 1 ? p[(ir + 1) * nt + it] : p0,
           ir > 0      ? p[(ir - 1) * nt + it] : p0,
-          it < nt = 1 ? p[ir * nt + it + 1] : p[ir * nt + 0],
+          it < nt - 1 ? p[ir * nt + it + 1] : p[ir * nt + 0],
           it > 0      ? p[ir * nt + it - 1] : p[ir * nt + nt - 1]};
-    float area = (r[ir+2]**2 - r[ir+1]**2) / 2 * (t[it+2] - t[it+1])
-    bxArea = b[ir, it] * area
-    pNext[ir, it] = laplace_point_iter(pStencil, cStencil, bxArea, 1)
+    float area = (r[ir+2]*r[ir+2] - r[ir+1]*r[ir+1]) / 2
+               * (t[it+2] - t[it+1]);
+    float bxArea = b[ir * nt + it] * area;
+    pNext[ir * nt + it] = laplace_point_iter(pStencil, cStencil, bxArea, 1);
+}
+
+__global__
+void laplace_iter(float * pNext, float * p, float * b, float * r, float * t,
+                  int nr, int nt)
+{
+    int ir = blockIdx.x * blockDim.x + threadIdx.x;
+    int it = blockIdx.y * blockDim.y + threadIdx.y;
+    if (ir < nr and it < nt)
+        laplace_iter_i(pNext, p, b, r, t, ir, it, nr, nt);
+}
+
+float * laplace_iters(float * pNext, float * p, float * b, float * r, float * t,
+                      int nr, int nt, int nIters)
+{
+    for (int iIter = 0; iIter < nIters; ++iIter) {
+        laplace_iter
+            <<<dim3(ceil(nr / 16.), ceil(nt / 16.)), dim3(16, 16)>>>
+            (pNext, p, b, r, t, nr, nt);
+        float * tmp = p;
+        p = pNext;
+        pNext = tmp;
+    }
+    return p;
 }
